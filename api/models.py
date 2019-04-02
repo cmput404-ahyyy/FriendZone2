9ee0ac09cf1a864e7e728eb3b307693073bd31c0
Hamdi Yusuf <hyusuf63@gmail.com>
2019-04-02 17:57:54 -0600

    fixed

--- api/models.py
+++ api/models.py
@@ -107,7 +107,7 @@ class Image(models.Model):
 
 class Node(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    url=models.URLField(blank=True)
+    url=models.URLField()
     username=models.CharField(max_length=32, blank=True)
     password=models.CharField(max_length=32, blank=True)
     sharePosts=models.BooleanField()
