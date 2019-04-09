626032d46ba6b4977fcf9ef652eef851cdc44c83
Hamdi Yusuf <hyusuf63@gmail.com>
2019-04-08 20:23:04 -0600

    new

--- api/views.py
+++ api/views.py
@@ -875,12 +875,20 @@ def respond_to_friend_request(request):
             print("came in try in copy")
             requester = Author.objects.get(url=data.get('from_author'))
             requestee,created = Author.objects.get_or_create(url=data.get('to_author')['url'],username=data.get('to_author')['username'],hostName=data.get('to_author')['hostName'])
+            print(requester)
+            print(requestee)
             existing_request = FriendRequest.objects.get(to_author=requestee, from_author=requester)
             make_them_friends(requester, requestee, existing_request)
+            print(requester)
+            print(requestee)
+            print("after make")
         except:
             print("came in except in copy")
             requester = Author.objects.get(url=data.get('from_author'))
             requestee,created = Author.objects.get_or_create(url=data.get('to_author')['url'],username=data.get('to_author')['username'],hostName=data.get('to_author')['hostName'])
+            print(requester)
+            print(requestee)
+            
             temp_dict = {"to_author" :requestee , "from_author":requester}
             serializer = FriendsSerializer(data=temp_dict)
             serializer.create(temp_dict)
