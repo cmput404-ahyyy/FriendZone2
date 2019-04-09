fdedb53b9418290d82e58e4c41d11a677bed5539
Hamdi Yusuf <hyusuf63@gmail.com>
2019-04-08 18:26:36 -0600

    new commit

--- api/views.py
+++ api/views.py
@@ -868,12 +868,15 @@ def respond_to_friend_request(request):
 
     if data.get("accepted"):
         requester_id = data.get("from_author")
-        requestee_id = data.get("to_author")
+        requestee_id = data.get("to_author")['author_id']
         try:
+            print("came in try in master")
+           
             existing_request = FriendRequest.objects.get(to_author=requestee_id, from_author=requester_id)
             requester = Author.objects.get(pk=requester_id)
             requestee = Author.objects.get(pk=requestee_id)
         except:
+            print("came in except in master")
             requester = Author.objects.get('from_author')
             requestee = Author.objects.create(url=data.get('to_author')['url'],username=data.get('to_author')['username'],hostName=data.get('to_author')['hostName'])
 
